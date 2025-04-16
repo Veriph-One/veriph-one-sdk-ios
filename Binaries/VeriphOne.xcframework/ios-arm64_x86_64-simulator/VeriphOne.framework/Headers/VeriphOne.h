a8eff59a0035fde7ef7c3915ee093d107773d3f6
c-sleanshot <c@sleanshot.com>
2025-04-16 16:34:08 -0600

    Added v1.2.0

--- Binaries/VeriphOne.xcframework/ios-arm64_x86_64-simulator/VeriphOne.framework/Headers/VeriphOne.h
+++ Binaries/VeriphOne.xcframework/ios-arm64_x86_64-simulator/VeriphOne.framework/Headers/VeriphOne.h
@@ -1,10 +1,3 @@
-//
-//  VeriphOne.h
-//  VeriphOne
-//
-//  Created by Marco Garcia on 28/05/24.
-//
-
 #import <Foundation/Foundation.h>
 
 //! Project version number for VeriphOne.
@@ -13,6 +6,5 @@ FOUNDATION_EXPORT double VeriphOneVersionNumber;
 //! Project version string for VeriphOne.
 FOUNDATION_EXPORT const unsigned char VeriphOneVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <VeriphOne/PublicHeader.h>
-
-
+// In this header, you should import all the public headers of your framework
+// using statements like #import <VeriphOne/PublicHeader.h>
